30b02d66096c9d5c123ed678cca5af2feef99b86
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-03-18 17:22:30 +0900

    add: listBooks

--- src/main/main.go
+++ src/main/main.go
@@ -11,12 +11,27 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "hello!")
 }
 
+func listBooks(c echo.Context) error {
+	bookTitle := c.QueryParam("title")
+	bookPublishedAt := c.QueryParam("published-at")
+
+	return c.String(http.StatusOK, fmt.Sprintf(
+		"The book title is %s\nThe book is published at %s", bookTitle, bookPublishedAt,
+	))
+
+	// http://localhost:8000/books?title=title&published-at=published-at
+
+	// The book title is title
+	// The book is published at published-at
+}
+
 func main() {
 	fmt.Println("Server started:")
 
 	e := echo.New()
 
 	e.GET("/", hello)
+	e.GET("/books", listBooks)
 
 	e.Start(":8000")
 }
